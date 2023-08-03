bc96775bc36c8edf1180578df61578630cc7bf28
Mohammad Omar <64026794+anaspui@users.noreply.github.com>
2023-08-03 15:47:34 +0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ["http://localhost:3000", "http://localhost:8080", "http://localhost:4200"]
   });
   await app.listen(8000);
 }
