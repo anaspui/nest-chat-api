c3acdd35b4eca8d8a2a9c6a1de4908c8bcc800cd
Mohammad Omar <64026794+anaspui@users.noreply.github.com>
2023-08-03 20:52:52 +0600

    PORT

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as process from "process";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ["http://localhost:3000", "http://localhost:8080", "http://localhost:4200"]
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:8080",
+      "http://localhost:4200",
+    ],
   });
-  await app.listen(8000);
+  await app.listen(process.env.PORT, "0.0.0.0");
 }
 bootstrap();
