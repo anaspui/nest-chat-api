e1776ef549314d1939de65af79ee70afe4fd904f
Mohammad Omar <64026794+anaspui@users.noreply.github.com>
2023-08-03 21:39:57 +0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
         "http://localhost:3000",
         "http://localhost:8080",
         "http://localhost:4200",
-        "https://chat-app-three-woad.vercel.app/",
+        "https://chat-app-anaspui.vercel.app",
       ],
     })
   );
