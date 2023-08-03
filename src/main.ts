3811426db35d3c8573501d2327f58f8590e43e74
Mohammad Omar <64026794+anaspui@users.noreply.github.com>
2023-08-03 21:30:28 +0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as process from "process";
+const cors = require("cors");
+const express = require("express");
+const app = express();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:8080",
-      "http://localhost:4200",
-    ],
-  });
+  app.use(
+    cors({
+      origin: [
+        "http://localhost:3000",
+        "http://localhost:8080",
+        "http://localhost:4200",
+        "https://chat-app-three-woad.vercel.app/",
+      ],
+    })
+  );
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
