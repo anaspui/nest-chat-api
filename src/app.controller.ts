9e65c03e158bb222ac35f7adfa0972c0910ba85e
Mohammad Omar <64026794+anaspui@users.noreply.github.com>
2023-08-03 21:02:51 +0600

    Routing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,20 @@
-import { Body, Controller, Post, Get, Options} from '@nestjs/common';
-import { ChatService } from './chat.service';
+import { Body, Controller, Post, Get, Options } from "@nestjs/common";
+import { ChatService } from "./chat.service";
 
-@Controller('api')
+@Controller()
 export class AppController {
   constructor(private chatService: ChatService) {}
-
-  @Post('messages')
+  @Get("")
+  hell() {
+    return "Hello World!";
+  }
+  @Post("api/messages")
   async messages(@Body() username: string, @Body() message: string) {
-    this.chatService.trigger('chat', 'message', { username, message });
+    this.chatService.trigger("chat", "message", { username, message });
     return [];
   }
-   @Options('messages')
-  async Omessages(@Body() username: string, @Body() message: string) {
-    this.chatService.trigger('chat', 'message', { username, message });
-    return [];
-  }
-  @Get('hello')
-  hello(){
-    return 'Hello World!';
+  @Get("api/hello")
+  hello() {
+    return "Hello World!";
   }
 }
