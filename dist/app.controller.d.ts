3fe183f10c23c456cffddc652c27aac1ac558741
Mohammad Omar <64026794+anaspui@users.noreply.github.com>
2023-08-04 20:04:01 +0600

    .gitignore

--- dist/app.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { ChatService } from './chat.service';
-export declare class AppController {
-    private chatService;
-    constructor(chatService: ChatService);
-    messages(username: string, message: string): Promise<any[]>;
-}
--- dist/chat.service.d.ts
+++
@@ -1,6 +0,0 @@
-import * as Pusher from 'pusher';
-export declare class ChatService {
-    pusher: Pusher;
-    constructor();
-    trigger(channel: string, event: string, data: any): Promise<void>;
-}
